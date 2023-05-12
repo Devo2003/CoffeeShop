eca412bdc8dd161103aea4a7c2d698b858f61987
Devo2003 <93745734+Devo2003@users.noreply.github.com>
2023-05-11 22:48:27 -0500

    added code that gives me total change

--- CafeShop.cpp
+++ CafeShop.cpp
@@ -3,11 +3,10 @@
 #include <string>
 using namespace std;
 
-
-
 const string ItemNames[5] = {"Expresso", "Mocha", "Americano", "Latte", "Tea" };
-double prices[5] = {2.45, 5.66, 7.24, 3.99, 4.44};
+float price[5] = {2.45, 5.66, 7.24, 3.99, 4.44};
 
+double change;
 int balance;
 int dollars;
 int quarters;
@@ -17,11 +16,6 @@ int pennies;
 
 int main()
 {
-    
-
-    cout << ItemNames[1] << std::endl;
-
-
     //10 bucks 
     balance = 10;
     //coin variables
@@ -30,24 +24,35 @@ int main()
     dimes = 0;
     nickles = 0;
     pennies = 0;
+    //leftover change variable
+    change = 0.00;
 
+    for (int i = 0; i < 5; i++)
+    {
+        price[i];
+        change = balance - price[i];
+        //form = change;
+
+        double x;
+        
+        cout <<"You bought a: "<< ItemNames[i] << endl;
+        cout << "Which costed: " << price[i] << endl;
+        cout << "You payed with: " << balance << endl;
+        cout << "Your change total is: " << change << endl;
+        cout << "---------------------------" << endl;
+
+        x = change *100 ;
+
+        cout << "Here is your change in: " << endl;
+        cout << dollars << " Dollars" << endl;
+        cout << quarters << x/25 << " Quarters" << endl;
+        cout << dimes << x/10 << " Dimes" << endl;
+        cout << nickles << x/5 << " Nickles" << endl;
+        cout << pennies << x/1 <<" Pennies" << endl;
+        
+    }
+
+    //cout << ((double) a / 2); // typecasting
     
-
-
-
-    cout << balance - prices[1] << endl;
-
     
-
-    cout << "Here is your change in: " << endl;
-    //cout << dollars << " Dollars" << endl;
-    //cout << quarters << " Quarters" << endl;
-    //cout << dimes << " Dimes" << endl;
-    //cout << nickles << " Nickles" << endl;
-    //cout << pennies << " Pennies" << endl;
-    
-
-
-
-
 }
